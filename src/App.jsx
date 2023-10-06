507471f977488283de627d154074acb5331f85d1
abhishekb740 <abhishekb740@gmail.com>
2023-10-06 11:28:43 +0530

    NFT Generator and Validator repo initialized

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      NFT Certificate Generator and Validator
+    </>
+  )
+}
+
+export default App
