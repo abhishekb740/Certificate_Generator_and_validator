4a29eff8203a213690dad3d2ae46b5f8db88edd0
Srushtii86 <srushtikp86@gmail.com>
2023-10-26 19:42:14 +0530

    css

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,29 @@ import User from './components/User';
 import Navbar from './components/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (
     <>
     <BrowserRouter>
     <Navbar/>
+    
+    <Carousel/>
+    
       <Routes>
       <Route path='/' element = {<Organisation/>}/>
       <Route path='/user' element = {<User/>}/>
       </Routes>
+
+    <Footer/>
     </BrowserRouter>
+
+    
 
     </>
   );
